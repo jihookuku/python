ee656308ea2e8efc5bfe2d3aa3fd1bcfbb4da9af
김지훈 <zer0box@naver.com>
2025-09-10 11:24:24 +0900

    최초

---
+++ 02_string.py
@@ -0,0 +1,11 @@
+# 문자열은 '' 나 "" 모두 사용 가능
+name='kim ji-hoon'
+content = "My Content"
+
+# 여러줄의 문자열을 변수에 담을때
+multi = '''this is multi line string
+this is second line'''
+
+print('name:'+name)
+print('content:'+content)
+print('multi:'+multi)
